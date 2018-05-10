2b97e4caa18be36905cac188076f986529377334
patrick <pdelossantos49@gmail.com>
2018-05-10 14:56:12 +0800

    aha

--- config.php
+++ config.php
@@ -1,28 +1,28 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost/bag_hag/');
+define('HTTP_SERVER', 'https://baghag.herokuapp.com/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/bag_hag/');
+define('HTTPS_SERVER', 'https://baghag.herokuapp.com/');
 
 // DIR
-define('DIR_APPLICATION', 'C:/xamp7/htdocs/bag_hag/catalog/');
-define('DIR_SYSTEM', 'C:/xamp7/htdocs/bag_hag/system/');
-define('DIR_IMAGE', 'C:/xamp7/htdocs/bag_hag/image/');
-define('DIR_LANGUAGE', 'C:/xamp7/htdocs/bag_hag/catalog/language/');
-define('DIR_TEMPLATE', 'C:/xamp7/htdocs/bag_hag/catalog/view/theme/');
-define('DIR_CONFIG', 'C:/xamp7/htdocs/bag_hag/system/config/');
-define('DIR_CACHE', 'C:/xamp7/htdocs/bag_hag/system/storage/cache/');
-define('DIR_DOWNLOAD', 'C:/xamp7/htdocs/bag_hag/system/storage/download/');
-define('DIR_LOGS', 'C:/xamp7/htdocs/bag_hag/system/storage/logs/');
-define('DIR_MODIFICATION', 'C:/xamp7/htdocs/bag_hag/system/storage/modification/');
-define('DIR_UPLOAD', 'C:/xamp7/htdocs/bag_hag/system/storage/upload/');
+define('DIR_APPLICATION', 'https://baghag.herokuapp.com/catalog/');
+define('DIR_SYSTEM', 'https://baghag.herokuapp.com/system/');
+define('DIR_IMAGE', 'https://baghag.herokuapp.com/image/');
+define('DIR_LANGUAGE', 'https://baghag.herokuapp.com/catalog/language/');
+define('DIR_TEMPLATE', 'https://baghag.herokuapp.com/catalog/view/theme/');
+define('DIR_CONFIG', 'https://baghag.herokuapp.com/system/config/');
+define('DIR_CACHE', 'https://baghag.herokuapp.com/system/storage/cache/');
+define('DIR_DOWNLOAD', 'https://baghag.herokuapp.com/system/storage/download/');
+define('DIR_LOGS', 'https://baghag.herokuapp.com/system/storage/logs/');
+define('DIR_MODIFICATION', 'https://baghag.herokuapp.com/system/storage/modification/');
+define('DIR_UPLOAD', 'https://baghag.herokuapp.com/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'db_bag_hag');
+define('DB_HOSTNAME', 'us-cdbr-iron-east-04.cleardb.net');
+define('DB_USERNAME', 'bef8e19ac0c07c');
+define('DB_PASSWORD', '5a79c2c9');
+define('DB_DATABASE', 'heroku_e5a7ce05d01ad5f');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
