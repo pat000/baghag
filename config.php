7d7964c5c60bcff78997f0f217a2aff9d10e72e2
patrick <pdelossantos49@gmail.com>
2018-05-10 15:06:40 +0800

    aha

--- config.php
+++ config.php
@@ -6,17 +6,17 @@ define('HTTP_SERVER', 'https://baghag.herokuapp.com/');
 define('HTTPS_SERVER', 'https://baghag.herokuapp.com/');
 
 // DIR
-define('DIR_APPLICATION', '/catalog/');
-define('DIR_SYSTEM', '/system/');
-define('DIR_IMAGE', '/image/');
-define('DIR_LANGUAGE', '/catalog/language/');
-define('DIR_TEMPLATE', '/catalog/view/theme/');
-define('DIR_CONFIG', '/system/config/');
-define('DIR_CACHE', '/system/storage/cache/');
-define('DIR_DOWNLOAD', '/system/storage/download/');
-define('DIR_LOGS', '/system/storage/logs/');
-define('DIR_MODIFICATION', '/system/storage/modification/');
-define('DIR_UPLOAD', '/system/storage/upload/');
+define('DIR_APPLICATION', '/app/catalog/');
+define('DIR_SYSTEM', '/app/system/');
+define('DIR_IMAGE', '/app/image/');
+define('DIR_LANGUAGE', '/app/catalog/language/');
+define('DIR_TEMPLATE', '/app/catalog/view/theme/');
+define('DIR_CONFIG', '/app/system/config/');
+define('DIR_CACHE', '/app/system/storage/cache/');
+define('DIR_DOWNLOAD', '/app/system/storage/download/');
+define('DIR_LOGS', '/app/system/storage/logs/');
+define('DIR_MODIFICATION', '/app/system/storage/modification/');
+define('DIR_UPLOAD', '/app/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
