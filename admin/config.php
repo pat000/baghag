ceddc2058560daf31a78228f1cbe3a1ca9abcd9b
patrick <pdelossantos49@gmail.com>
2018-05-10 16:02:44 +0800

    ehe

--- admin/config.php
+++ admin/config.php
@@ -1,31 +1,31 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost/bag_hag/admin/');
-define('HTTP_CATALOG', 'http://localhost/bag_hag/');
+define('HTTP_SERVER', 'https://baghag.herokuapp.com/admin/');
+define('HTTP_CATALOG', 'https://baghag.herokuapp.com/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/bag_hag/admin/');
-define('HTTPS_CATALOG', 'http://localhost/bag_hag/');
+define('HTTPS_SERVER', 'https://baghag.herokuapp.com/admin/');
+define('HTTPS_CATALOG', 'https://baghag.herokuapp.com/');
 
 // DIR
-define('DIR_APPLICATION', 'C:/xamp7/htdocs/bag_hag/admin/');
-define('DIR_SYSTEM', 'C:/xamp7/htdocs/bag_hag/system/');
-define('DIR_IMAGE', 'C:/xamp7/htdocs/bag_hag/image/');
-define('DIR_LANGUAGE', 'C:/xamp7/htdocs/bag_hag/admin/language/');
-define('DIR_TEMPLATE', 'C:/xamp7/htdocs/bag_hag/admin/view/template/');
-define('DIR_CONFIG', 'C:/xamp7/htdocs/bag_hag/system/config/');
-define('DIR_CACHE', 'C:/xamp7/htdocs/bag_hag/system/storage/cache/');
-define('DIR_DOWNLOAD', 'C:/xamp7/htdocs/bag_hag/system/storage/download/');
-define('DIR_LOGS', 'C:/xamp7/htdocs/bag_hag/system/storage/logs/');
-define('DIR_MODIFICATION', 'C:/xamp7/htdocs/bag_hag/system/storage/modification/');
-define('DIR_UPLOAD', 'C:/xamp7/htdocs/bag_hag/system/storage/upload/');
-define('DIR_CATALOG', 'C:/xamp7/htdocs/bag_hag/catalog/');
+define('DIR_APPLICATION', '/app/admin/');
+define('DIR_SYSTEM', '/app/system/');
+define('DIR_IMAGE', '/app/image/');
+define('DIR_LANGUAGE', '/app/admin/language/');
+define('DIR_TEMPLATE', '/app/admin/view/template/');
+define('DIR_CONFIG', '/app/system/config/');
+define('DIR_CACHE', '/app/system/storage/cache/');
+define('DIR_DOWNLOAD', '/app/system/storage/download/');
+define('DIR_LOGS', '/app/system/storage/logs/');
+define('DIR_MODIFICATION', '/app/system/storage/modification/');
+define('DIR_UPLOAD', '/app/system/storage/upload/');
+define('DIR_CATALOG', '/app/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'db_bag_hag');
+define('DB_HOSTNAME', 'us-cdbr-iron-east-04.cleardb.net');
+define('DB_USERNAME', 'bef8e19ac0c07c');
+define('DB_PASSWORD', '5a79c2c9');
+define('DB_DATABASE', 'heroku_e5a7ce05d01ad5f');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
